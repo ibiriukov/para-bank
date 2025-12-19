76ea8830bde9387a0117c24b8f1b8fba44c03841
Ievgen Biriukov <71152242+ibiriukov@users.noreply.github.com>
2025-12-19 10:19:59 -0500

    Update test_transfer_funds.py

--- tests/test_transfer_funds.py
+++ tests/test_transfer_funds.py
@@ -45,7 +45,7 @@ def test_open_new_account(page, account_overview_page):
     account_overview_page=open_new_account_page.select_tab_in_side_panel("Accounts Overview")
     assert assert_is_true(account_overview_page.get_text_of_h1(), "Accounts Overview")
     assert assert_is_true(account_overview_page.get_number_of_accounts(), 2)
-    time.sleep(1)
+    time.sleep(1)  # Used for demo purposes only
     assert account_overview_page.verify_amount_value_based_col_name(account_number_checking, "$1000.00", "Balance")
     assert account_overview_page.verify_amount_value_based_col_name(account_number_checking, "$1000.00", "Available Amount")
 
@@ -74,7 +74,7 @@ def test_transfer_funds(page, account_overview_page):
     account_overview_page = open_new_account_page.select_tab_in_side_panel("Accounts Overview")
     assert assert_is_true(account_overview_page.get_text_of_h1(), "Accounts Overview")
     assert assert_is_true(account_overview_page.get_number_of_accounts(), 2)
-    time.sleep(1)
+    time.sleep(1) # Used for demo purposes only
     assert account_overview_page.verify_amount_value_based_col_name(account_number_checking, "$1000.00", "Balance")
     assert account_overview_page.verify_amount_value_based_col_name(account_number_checking, "$1000.00",
                                                                     "Available Amount")
@@ -89,7 +89,7 @@ def test_transfer_funds(page, account_overview_page):
     assert assert_is_true(transfer_funds_page.transfer_from_to(account_number_checking, account_number_savings), "Transfer Complete!")
     account_overview_page=transfer_funds_page.select_tab_in_side_panel("Accounts Overview")
     assert assert_is_true(account_overview_page.get_text_of_h1(), "Accounts Overview")
-    time.sleep(1)
+    time.sleep(1) # Used for demo purposes only
     assert account_overview_page.verify_amount_value_based_col_name(account_number_checking, "$700.00", "Balance")
     assert account_overview_page.verify_amount_value_based_col_name(account_number_checking, "$700.00",
                                                                     "Available Amount")
@@ -98,4 +98,4 @@ def test_transfer_funds(page, account_overview_page):
     assert account_overview_page.verify_amount_value_based_col_name(account_number_savings, "$300.00", "Available Amount")
     assert assert_is_true(account_overview_page.get_total_amount(), "Total$1000.00")
 
-    time.sleep(2)
+    time.sleep(2) # Used for demo purposes only
