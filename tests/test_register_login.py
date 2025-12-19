9db1a09d102574be5ca90740a259d265fe734e54
Ievgen Biriukov <71152242+ibiriukov@users.noreply.github.com>
2025-12-19 10:18:44 -0500

    Update test_register_login.py

--- tests/test_register_login.py
+++ tests/test_register_login.py
@@ -3,5 +3,5 @@ import time
 
 def test_register_login(page, account_overview_page):
     assert account_overview_page.get_text_of_h1() == "Accounts Overview"
-    time.sleep(1)
+    time.sleep(1)  # Used for testing purposes only
 
